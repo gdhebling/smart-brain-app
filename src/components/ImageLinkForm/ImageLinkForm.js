3d976ed3fede823bd5c050699579353ba85da3f9
Gui Hebling <guilherme.durso@outlook.com>
2020-11-26 12:18:36 +0100

    Change button text justify

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -13,9 +13,9 @@ export const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
                 </p>
             </div>
             <div className="center">
-                <div className="form center pa4 br3 shadow-5">
+                <div className="form center pa4-ns br3 shadow-5">
                     <input className="f4 pa2 w-70 center" type="text" onChange={onInputChange} />
-                    <button className="f4 w-30 grow link ph3 pv2 dib white bg-light-purple"
+                    <button className="f4 w-30 grow link ph3 pv2 dib white bg-light-purple flex justify-center"
                         onClick={onButtonSubmit}>Detect</button>
                 </div>
             </div>
