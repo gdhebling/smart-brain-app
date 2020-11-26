019a6e90cbe7ba1efbd0fd083acf2a2d0b018ced
Gui Hebling <guilherme.durso@outlook.com>
2020-11-26 11:54:52 +0100

    Small change on rank text username

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -4,7 +4,7 @@ export const Rank = () => {
     return (
         <div>
             <div className="white f3">
-                {"Andrei, your current rank is..."}
+                {"Hi, your current rank is..."}
             </div>
             <div className="white f1">
                 {"#5"}
