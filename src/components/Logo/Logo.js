5601ee614c84f613efb542a2c01a925ef699932d
Gui Hebling <guilherme.durso@outlook.com>
2021-01-24 22:46:27 +0100

    Change margin top of logo div

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,16 +1,20 @@
-import React from 'react'
-import brain from './brain-logo.png'
-import Tilt from 'react-tilt'
-import './Logo.css'
+import React from "react";
+import brain from "./brain-logo.png";
+import Tilt from "react-tilt";
+import "./Logo.css";
 
 export const Logo = () => {
-    return (
-        <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max: 35 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner pa3">
-                    <img style={{ paddingTop: "0.5rem" }} src={brain} alt="brain logo" />
-                </div>
-            </Tilt>
+  return (
+    <div className="ma4 mt4">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 35 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner pa3">
+          <img style={{ paddingTop: "0.5rem" }} src={brain} alt="brain logo" />
         </div>
-    )
-}
+      </Tilt>
+    </div>
+  );
+};
