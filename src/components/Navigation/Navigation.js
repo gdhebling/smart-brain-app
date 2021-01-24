db06c7083199baba6d0fddbe9622a2409202fe91
Gui Hebling <guilherme.durso@outlook.com>
2021-01-24 22:40:00 +0100

    Update Navbar style

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,31 +1,40 @@
-import React from 'react'
+import React from "react";
 
 export const Navigation = ({ onRouteChange, isSignedIn }) => {
-    if (isSignedIn) {
-        return (
-            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <p
-                    onClick={() => onRouteChange('signin')}
-                    className='f3 link dim black underline pa3 pointer'>
-                    Sign Out
-                </p>
-            </nav>
-        )
-    } else {
-        return (
-            <nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
-                <p
-                    onClick={() => onRouteChange('signin')}
-                    className='f3 link dim black underline pa3 pointer'>
-                    Sign In
-                </p>
-                <p
-                    onClick={() => onRouteChange('register')}
-                    className='f3 link dim black underline pa3 pointer'>
-                    Register
-                </p>
-            </nav>
-        )
-    }
-
-}
+  if (isSignedIn) {
+    return (
+      <nav
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          position: "absolute",
+          width: "100%",
+        }}
+      >
+        <p
+          onClick={() => onRouteChange("signin")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign Out
+        </p>
+      </nav>
+    );
+  } else {
+    return (
+      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
+        <p
+          onClick={() => onRouteChange("signin")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Sign In
+        </p>
+        <p
+          onClick={() => onRouteChange("register")}
+          className="f3 link dim black underline pa3 pointer"
+        >
+          Register
+        </p>
+      </nav>
+    );
+  }
+};
